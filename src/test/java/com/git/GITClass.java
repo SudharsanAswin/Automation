bfd88da0034fceec14ae6a7acd9ad6e29a3af285
SudharsanAswin <muraligaja.mgl@gamil.com>
2022-05-31 15:37:11 +0530

    new

--- src/test/java/com/git/GITClass.java
+++ src/test/java/com/git/GITClass.java
@@ -2,28 +2,7 @@ package com.git;
 
 public class GITClass {
 
-	public void method1() {
-		System.out.println(1);
-	}
-
-	public void method2() {
-		System.out.println(2);
-
-	}
-	public void method4() {
-		System.out.println(2);
-		
-	}
-	public void method5() {
-		System.out.println(2);
-		
-	}
-	public void method6() {
-		System.out.println(2);
-		
-	}
-	public void method7() {
-		System.out.println(2);
+	 public static void main(String[] args) {
 		
 	}
 
